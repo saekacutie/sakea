0ebfdfd8fd505c33336018fbff7f4604819a5533
くんジェット <130436764+saekacutie@users.noreply.github.com>
2026-05-08 13:48:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1183,8 +1183,9 @@ def get_temp_number():
     number_found = False
 
     # ── sms-online.co ──
+    if svc_key == "smsonline":
         try:
-        r = requests.get("https://sms-online.co/", timeout=15)
+            r = requests.get("https://sms-online.co/", timeout=15)
         if r.status_code == 200:
             soup = BeautifulSoup(r.text, 'html.parser')
             numbers = []
